690c89e4a53235cb1acc3bf7b74e4144d0485a9e
1kush-patel <kushpatel123.kp14@gmail.com>
2024-07-26 15:13:18 -0400

    project structure setup

---
+++ image-service/src/test/java/humber/kush/imageservice/ImageServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package humber.kush.imageservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImageServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
